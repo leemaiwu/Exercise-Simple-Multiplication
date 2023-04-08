f5802c11c029f32be786f2836dd4578663dbf009
Laura Wu <leemaiwu@gmail.com>
2023-04-08 16:14:26 -0600

    Completed codewars problem

---
+++ solution.js
@@ -0,0 +1,10 @@
+function simpleMultiplication(number) {
+    if (number % 2 === 0) {
+        number = number * 8
+    } else {
+        number = number * 9
+    }
+    return number
+} 
+
+console.log(simpleMultiplication(3))
